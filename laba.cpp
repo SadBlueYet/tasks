1600270272d69c0fbaccb5dfad40d72fe337dfc0
SadBlueYet <88283979+SadBlueYet@users.noreply.github.com>
2022-11-30 13:48:07 +0700

    Update laba.cpp

--- laba.cpp
+++ laba.cpp
@@ -1,35 +1,38 @@
-﻿#include <iostream>
+#include <iostream>
 #include <stdio.h>
 #include <math.h>
 void outputArray();
 void isMathEexpectation(int* arr);
-void disp(int* arr, double
-	mathExpectation);
+void disp(int* arr, double mathExpectation);
+void standardDeviation(int* arr, double mathExpectation);
+void normalization(int* arr, double mathExpectation);
 int main()
 {
 	outputArray();
-	
+
 }
 void outputArray() {
 	const int arrLength = 10;
 	int arrayNumbers[arrLength];
-	
+
 	for (int i = 0; i < arrLength; i++) {
 		std::cin >> arrayNumbers[i];
-		
+
 	}
 	isMathEexpectation(arrayNumbers);
-	
-	
+
+
 }
 void isMathEexpectation(int* arr) {
-	double mathEexpectation = 0;
+	double mathExpectation = 0;
 	for (int i = 0; i < 10; i++) {
-		mathEexpectation += arr[i];
+		mathExpectation += arr[i];
 	}
-	mathEexpectation /= 10;
-	std::cout << mathEexpectation  << '\n';
-	disp(arr, mathEexpectation);
+	mathExpectation /= 10;
+	std::cout << "Math.Eexpectation: " << mathExpectation << '\n';
+	disp(arr, mathExpectation);
+	standardDeviation(arr, mathExpectation);
+	normalization(arr, mathExpectation);
 }
 void disp(int* arr, double mathExpectation) {
 	double disp = 0;
@@ -37,5 +40,25 @@ void disp(int* arr, double mathExpectation) {
 		disp += pow(arr[i] - mathExpectation, 2);
 	}
 	disp = sqrt(disp / 9);
-	std::cout << disp;
+	std::cout << "Dispertion: " << disp << '\n';
+}
+void standardDeviation(int* arr, double mathExpectation) {
+	double deviation = 0;
+	for (int i = 0; i < 10; i++) {
+		deviation += pow(arr[i] - mathExpectation, 2);
+	}
+	deviation = sqrt(deviation / 10);
+	std::cout << "Deviation: " << deviation << '\n';
+}
+void normalization(int* arr, double mathExpectation) {
+	double normalization;
+	double max = arr[0];
+	double min = arr[0];
+	for (int i = 0; i < 10; i++) {
+		if (max < arr[i]) max = arr[i];
+		if (min > arr[i]) min = arr[i];
+
+	}
+	normalization = (mathExpectation - min) / (max - min);
+	std::cout << "Normalization: " << normalization;
 }
